1d20658e665101df085e86c8aabb0754c9bf911a
unknown <umar.suhail@uvionicstech.com>
2018-12-18 12:14:10 +0530

    bedroom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Bedroom  from './components/Bedroom'
 
 class App extends Component {
   render() {
@@ -8,8 +9,10 @@ class App extends Component {
       <div className="App">
       <div className="content">
         <div className="row">
-          <div className="col-lg-9">dfgdgfghfhfghfg</div>
-          <div className="col-lg-3">hfgfhfghghjhgjhgjg</div>
+          <div className="col-lg-9">
+<Bedroom/>
+          </div>
+          {/* <div className="col-lg-3">hfgfhfghghjhgjhgjg</div> */}
         </div>
         </div>
       </div>
--- src/components/bedroom.js
+++ src/components/bedroom.js
@@ -1,43 +1,47 @@
-import React, { Component } from 'react'
-import Switch from '@material-ui/core/Switch';
-import '../css/rooms.css';
+import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import '../css/bedroom.css'
 export default class Bedroom extends Component {
     constructor(props){
         super(props)
         this.state = {
             auth: true,
             anchorEl: null,
+            auth2: true,
         };
     }
    
      handleChange = event => {
     this.setState(state => ({ auth: !state.auth }));
      };
+     handleChange2 = event => {
+    this.setState(state => ({ auth2: !state.auth }));};
   render() {
-      const {auth,anchorEl} = this.state;
+      const {
+          auth,
+          auth2,anchorEl
+      } = this.state;
     return (
  <div className="row">
                 <div className = "control-panel col-lg-9" >
                      <h1></h1>
                  </div>
        <div className="control-panel col-lg-3">
-       <div className="row">
+       <div className="side-component">
         <Button onClick={this.handleChange} variant="contained" color="primary" className="primary ">Light</Button>
+        <Button onClick={this.handleChange2} variant="contained" color="primary" className="primary ">Fan</Button>
        </div>
-       <div className="row">
-
        </div>
-        <Button onClick={this.handleChange} variant="contained" color="primary" className="primary ">Light</Button>
-                <Button onClick={this.handleChange} variant="contained" color="primary" className="primary ">Fan</Button>
-                        <Button onClick={this.handleChange} variant="contained" color="primary" className="primary ">Music System</Button>
-       </div>
-      {auth && (
-          <div className="col bedroom">
-            <h1>h111</h1>
-          </div>
-      )
-      }
+             {auth && (
+                 <div className="col bedroom">
+                 <h1>Light On</h1>
+                </div>)
+            }
+  {auth2 && (
+                 <div className="col bedroom">
+                 <h1>Fan On</h1>
+                </div>)
+            }
  </div>
      
     )
