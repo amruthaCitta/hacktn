1bc1691b0e2fe675ad9d761dc74358d0c244372f
unknown <umar.suhail@uvionicstech.com>
2018-12-18 11:28:33 +0530

    bedroom

--- src/components/bedroom.js
+++ src/components/bedroom.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import Switch from '@material-ui/core/Switch';
 import '../css/rooms.css';
+import Button from '@material-ui/core/Button';
 export default class Bedroom extends Component {
-    state = {
-        auth: true,
-        anchorEl: null,
-    };
+    constructor(props){
+        super(props)
+        this.state = {
+            auth: true,
+            anchorEl: null,
+        };
+    }
+   
      handleChange = event => {
-         this.setState({
-             auth: event.target.checked
-         });
+    this.setState(state => ({ auth: !state.auth }));
      };
   render() {
       const {auth,anchorEl} = this.state;
@@ -19,11 +22,19 @@ export default class Bedroom extends Component {
                      <h1></h1>
                  </div>
        <div className="control-panel col-lg-3">
-        <Switch checked={auth} onChange={this.handleChange} aria-label="LoginSwitch" />
+       <div className="row">
+        <Button onClick={this.handleChange} variant="contained" color="primary" className="primary ">Light</Button>
+       </div>
+       <div className="row">
+
+       </div>
+        <Button onClick={this.handleChange} variant="contained" color="primary" className="primary ">Light</Button>
+                <Button onClick={this.handleChange} variant="contained" color="primary" className="primary ">Fan</Button>
+                        <Button onClick={this.handleChange} variant="contained" color="primary" className="primary ">Music System</Button>
        </div>
       {auth && (
           <div className="col bedroom">
-
+            <h1>h111</h1>
           </div>
       )
       }
