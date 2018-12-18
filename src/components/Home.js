5b493aec18100c3f119dbc16c7d63e689930b192
Amrutha Babu <amrutha.babu@citta.ai>
2018-12-18 12:24:43 +0530

    app.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../css/home.css';
+import '../css/home.css'; 
 import Button from './modal/Button';
 import '../css/common.css';
 
@@ -10,14 +10,19 @@ export default class Home extends Component {
         <div className="row no-padding">
           <div className="col-lg-9">
             <div className="all-room-btn">
-              <Button/>
+              <Button />
             </div>
-            <div className="home-structure">
+            <div className="home-structure row">
               <div className="bed-room col-lg-4">Bedroom</div>
+              <div className="col-lg-4"></div>
+              <div className="living-room col-lg-4">Living room</div>
+            </div>
+            <div className="row">
+
             </div>
           </div>
           <div className="col-lg-3">
-             
+            fdfdgdfgfdhgf
           </div>
         </div>
       </div>
