3bdde04d74dcf7f0319062bcdadbef48479ee57b
Amrutha Babu <amrutha.babu@citta.ai>
2018-12-18 12:13:27 +0530

    app.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../css/home.css';
+import Button from './modal/Button';
+import '../css/common.css';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <div className="row">
+        <div className="row no-padding">
           <div className="col-lg-9">
-dgdfgdfg
+            <div className="all-room-btn">
+              <Button/>
+            </div>
+            <div className="home-structure">
+              <div className="bed-room col-lg-4">Bedroom</div>
+            </div>
           </div>
           <div className="col-lg-3">
-dfgfdfgfdg
+             
           </div>
         </div>
       </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../css/navigation.css'; 
 
 export default class Navigation extends Component {
   render() {
     return (
-      <div>
+      <div className="navigation">
         
       </div>
     )
